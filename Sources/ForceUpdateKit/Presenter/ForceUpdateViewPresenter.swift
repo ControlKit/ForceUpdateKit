619f5b7027208b54f479a559bfcb5fa1b4b4eaf3
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-08-26 13:26:46 +0330

    Add icon to update presenter

--- Sources/ForceUpdateKit/Presenter/ForceUpdateViewPresenter.swift
+++ Sources/ForceUpdateKit/Presenter/ForceUpdateViewPresenter.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Maziar Saadatfar on 8/26/25.
 //
+import Foundation
+import UIKit
 public struct ForceUpdateViewPresenter {
     var config: ForceUpdateViewConfig
     public init(data: UpdateModel?, config: ForceUpdateViewConfig) {
@@ -16,6 +18,7 @@ public struct ForceUpdateViewPresenter {
             let version = getLocalizeString(localVersion) { self.config.versionText = version }
         if let localDescription = data?.description,
            let description = getLocalizeString(localDescription) { self.config.descriptionText = description }
+        if let icon = data?.icon, let url = URL(string: icon) { downloadImage(from: url) }
     }
     
     func getLocalizeString(_ localize: LocalString) -> String? {
@@ -27,5 +30,16 @@ public struct ForceUpdateViewPresenter {
             }
         }
         return localizeString.content
+    }
+    
+    func downloadImage(from url: URL) {
+        getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            self.config.updateImage = UIImage(data: data)
+        }
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
 }
