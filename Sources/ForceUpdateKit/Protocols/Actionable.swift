e4ad824573e65f82b4f942a01c35f4fe35804aa7
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-10-07 19:22:23 +0330

    Add action Id

--- Sources/ForceUpdateKit/Protocols/Actionable.swift
+++ Sources/ForceUpdateKit/Protocols/Actionable.swift
@@ -11,13 +11,14 @@ public protocol Actionable {
 public extension Actionable where Self: ForceUpdateViewModel {
     func setAction(_ action: ControlKitAction) {
         Task {
-            let request = ControlKitBase.ActionRequest(
+            var request = ControlKitBase.ActionRequest(
                 route: .force_update,
                 appId: serviceConfig.appId,
                 sdkVersion: forceUpdateKit_Version,
                 action: action,
                 itemId: self.response.data?.id ?? "",
             )
+            request.extraParameter = "\(request.itemId ?? "")"
             let result: Result<ActionResponse> = try await actionService.execute(request: request)
             
         }
